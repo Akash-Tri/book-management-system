a79c2be09b465feb83d2784baf06a27802de35aa
Akash Tripathi <tripathig0542@gmail.com>
2025-05-27 17:07:47 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,13 @@ function App() {
     <>
       <div className="search">
         <h2>Search Books</h2>
-        <div>
-          <input
-            type="text"
-            className="search-input"
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <button onClick={handleSearch}>Search</button>
-        </div>
+
+        <input
+          type="text"
+          className="search-input"
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
       <BookStore bookData={filteredBooks} />
     </>
